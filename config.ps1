da2b8b839f3f49f6b0fdd656678ba0629499dcb7
Markus Frosch <markus.frosch@icinga.com>
2019-04-02 16:52:02 +0200

    Update to 1.1.1b

--- config.ps1
+++ config.ps1
@@ -1,5 +1,8 @@
+# Tune Powershell TLS protocols
+$AllProtocols = [System.Net.SecurityProtocolType]'Tls11,Tls12'
+[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
 
-$global:OpenSSL_version = "1_1_0g"
+$global:OpenSSL_version = "1_1_1b"
 $global:OpenSSL_file = "OpenSSL_$OpenSSL_version.zip"
 $global:OpenSSL_sourcedir = "openssl-OpenSSL_$OpenSSL_version"
 $global:OpenSSL_url = "https://github.com/openssl/openssl/archive/$OpenSSL_file"
